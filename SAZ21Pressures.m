a21886bc605796bd527b4db6489158bb6eeb51af
Cathryn Wynn-Edwards <Cathryn.WynnEdwards@utas.edu.au>
2021-10-12 08:06:11 +1100

    added a folder for figures and the Aquadopp file to the pressure script

--- SAZ21Pressures.m
+++ SAZ21Pressures.m
@@ -8,6 +8,7 @@ cd(search_path)
 	files{1} = 'IMOS_DWM-SOTS_CPT_20190312_SAZ47_FV00_SAZ47-21-2019-SBE37-SM-4906-1000m_END-20200905_C-20210513.nc';
 	files{2} = 'IMOS_DWM-SOTS_CPT_20190312_SAZ47_FV00_SAZ47-21-2019-SBE37-SM-4907-2000m_END-20200905_C-20210513.nc';
     files{3} = 'IMOS_DWM-SOTS_PT_20190312_SAZ47_FV00_SAZ47-21-2019-TDR-2050-016371-3800m_END-20200601_C-20210513.nc';
+    files{4} = 'netCDF/IMOS_DWM-SOTS_AETVZ_20190320_SAZ47_FV01_SAZ47-21-2019-Aquadopp-Current-Meter-1200_END-20200903_C-20201112.nc';
 
 dep_start = ncreadatt(files{1}, '/', 'time_deployment_start');
 dep_end = ncreadatt(files{1}, '/', 'time_deployment_end');
@@ -26,23 +27,35 @@ for fn = files
     file = fn{1};
     disp(file);
     
-    pres = ncread(file, 'PRES');
+    try
+        pres = ncread(file, 'PRES');
+    catch
+        pres = ncread(file, 'PRES_REL');
+    end
     time = ncread(file, 'TIME') + datetime(1950,1,1);
-
+    instrument = ncreadatt(file, '/', 'instrument');
     msk = time > datetime_start & time < datetime_end;
-    
-    plot(time(msk), pres(msk));
-    
+        
     mn = mean(pres(msk));
     md = mode(pres(msk));
+    
+    plot(time(msk), pres(msk), 'DisplayName', sprintf('mode pres %4.0f %s' ,md,instrument));
     
     disp([mn md])
 
     %cur = sqrt(ncread(file, 'UCUR').^2 + ncread(file, 'VCUR').^2);
-    
+    %legend('Orientation','vertical','Location','bestoutside')
+    legend('Location','best')
 end
+depl = ncreadatt(files{1},'/','deployment_code');
 
+fig = figure(1);
+figurename = [depl '_pressure'];
 
+figure_path = 'C:\Users\cawynn\OneDrive - University of Tasmania\Documents\GitHub\saz-data-processing\figures';
+cd(figure_path)
+
+print(figurename, '-dpng');
 
 
 
