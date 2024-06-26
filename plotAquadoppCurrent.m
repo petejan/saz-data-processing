220e8a4226b129fe6d91de867699904c6618a48c
CathrynAWE <60637561+CathrynAWE@users.noreply.github.com>
2024-06-26 11:42:11 +1000

    created current figure for SAZ24

--- plotAquadoppCurrent.m
+++ plotAquadoppCurrent.m
@@ -3,7 +3,7 @@
 %file = 'data/IMOS_ABOS-ASFS_EVZ_20150322T001500Z_SOFS_FV01_SOFS-5-2015-Sea-Guard-200_END-20160420T041501Z_C-20161023T042146Z.nc';
 %file = 'data/IMOS_ABOS-ASFS_EVZ_20150322T001500Z_SOFS_FV01_SOFS-5-2015-Sea-Guard-800_END-20160420T040000Z_C-20161023T042334Z.nc';
 
-file = 'netCDF/IMOS_DWM-SOTS_AETVZ_20210424_SAZ47_FV01_SAZ47-23-2021-Aquadopp-Current-Meter-AQD-9897-1200m_END-_C-20220530.nc';
+file = 'netCDF/IMOS_DWM-SOTS_AEPV_20220501_SAZ47_FV00_SAZ47-24-2022-Aquadopp-2-MHz-AQD-9882-1215m_END-20230530_C-20230727.nc';
 
 %file = 'IMOS_DWM-SOTS_AETVZ_20180303_SAZ47_FV01_SAZ47-20-2018-Aquadopp-Current-Meter-AQD-5961-1200m_END-20190322_C-20190719.nc';
 
@@ -13,12 +13,12 @@ inst = ncreadatt(file, '/', 'instrument');
 
 TIME = ncread(file, 'TIME') + datenum(1950,1,1);
 
-VCUR = ncread(file, 'VCUR');
-qc_v = ncreadatt(file, 'VCUR', 'ancillary_variables');
-VCUR_qc = ncread(file, qc_v);
-UCUR = ncread(file, 'UCUR');
-qc_u = ncreadatt(file, 'UCUR', 'ancillary_variables');
-UCUR_qc = ncread(file, qc_u);
+VCUR = ncread(file, 'VCUR_MAG');
+%qc_v = ncreadatt(file, 'VCUR_MAG', 'ancillary_variables');
+%VCUR_qc = ncread(file, qc_v);
+UCUR = ncread(file, 'UCUR_MAG');
+%qc_u = ncreadatt(file, 'UCUR_MAG', 'ancillary_variables');
+%UCUR_qc = ncread(file, qc_u);
 
 roll = ncread(file, 'ROLL');
 pitch = ncread(file, 'PITCH');
@@ -28,7 +28,9 @@ cspd = sqrt(VCUR.^2 + UCUR.^2);
 
 figure(1)
 edges = 0:0.001:0.4;
-N = histcounts(cspd(VCUR_qc<=1), edges, 'Normalization', 'cdf');
+%N = histcounts(cspd(VCUR_qc<=1), edges, 'Normalization', 'cdf');
+% using FV00 file, which hasn't run through the IMOS tool box
+N = histcounts(cspd, edges, 'Normalization', 'cdf');
 plot(edges(1:end-1),N)
 current_90_idx = find(N>0.9,1);
 current_90 = edges(current_90_idx);
@@ -42,7 +44,10 @@ figure(4)
 
 edg_tilt = 0:0.2:20;
 %Ntilt = histcounts(tilt(VCUR_MAG_qc<=1), edg_tilt, 'Normalization', 'pdf');
-Ntilt = histcounts(tilt(VCUR_qc<=1), edg_tilt, 'Normalization', 'probability');
+% FV00 file
+%Ntilt = histcounts(tilt(VCUR_qc<=1), edg_tilt, 'Normalization', 'probability');
+Ntilt = histcounts(tilt, edg_tilt, 'Normalization', 'probability');
+
 
 plot(edg_tilt(1:end-1), Ntilt);
 ylabel('probability'); xlabel('tilt (deg)');
@@ -51,7 +56,9 @@ xlim([0 8]);
 grid(); title(sprintf('%s : %s @ %4.0f m',  ttl, inst, dpt))
 
 figure(5);
-plot(TIME(VCUR_qc<=1), cspd(VCUR_qc<=1)); grid on; datetick('x', 'keeplimits');
+%plot(TIME(VCUR_qc<=1), cspd(VCUR_qc<=1)); grid on; datetick('x', 'keeplimits');
+% FV00 file
+plot(TIME, cspd); grid on; datetick('x', 'keeplimits');
 title(sprintf('%s : %s @ %4.0f m',  ttl, inst, dpt))
 ylabel('current (m/s)');
 
